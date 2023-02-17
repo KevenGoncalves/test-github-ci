6ee2e69b4870b6a5bb1d9ac4af5fd336ba05e247
Keven Gonçalves <keven.jm.goncalves@gmail.com>
2023-02-17 13:13:15 +0200

    feat(app-server):add express as a server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.SERVER_PORT || 3030;
+
+app.use(express.json());
+app.listen(port, () =>
+  console.log(`[SERVER] - Running on http://localhost:${port}`)
+);
+
 function HelloWorld() {
   const variable = "Hello World";
   console.log(variable);
@@ -11,7 +20,8 @@ function TestFunc() {
   return variable;
 }
 
-HelloWorld();
-TestFunc();
+app.get("/", (req, res) => {
+  return res.json({ message: HelloWorld() });
+});
 
 module.exports = { HelloWorld, TestFunc };
